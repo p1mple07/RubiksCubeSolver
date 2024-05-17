dcd5b4a1c3184212b4c842589f5204e669b8b8fe
shashwat <shashwatskr2019@gmail.com>
2024-05-17 14:58:14 +0530

    initial commit with some files added

---
+++ CubeClass.h
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+unsigned int bitSelect(unsigned int face,int r,int l){
+    unsigned int temp = face&((1<<(r+1)) - (1<<l));// error can happen
+    return temp;
+}
+class cube
+{
+    unsigned int faces[6];
+    public:
+    // 0 white
+    // 1 yellow
+    // 2 blue
+    // 3 green 
+    // 4 red
+    // 5 orange
+    cube(){
+        faces[0] =  0;// white
+        faces[1] =  stoi("00010001000100010001000100010001", nullptr, 2);// yellow
+        faces[2] =  stoi("00100010001000100010001000100010", nullptr, 2);// blue this is towords observer
+        faces[3] =  stoi("00110011001100110011001100110011", nullptr, 2);// green
+        faces[4] =  stoi("01000100010001000100010001000100", nullptr, 2);// red
+        faces[5] =  stoi("01010101010101010101010101010101", nullptr, 2);// orange
+    }
+    // R,R',R2,L,U,D,B,F,
+    void R();
+    void R_prime();
+    void R2();
+    void L();
+    void L_prime();
+    void L2();
+    void print();
+};
---
+++ LeftMoves.cpp
@@ -0,0 +1,79 @@
+#include "CubeClass.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void cube::R(){
+    unsigned int temp = faces[4];
+    unsigned int rightMask = ((1<<24) - (1<<12));
+    faces[4] = faces[4]>>8;
+    temp = temp<<24;
+    faces[4] = temp|faces[4];// red center rotated
+
+    unsigned int Rblue = faces[2]&rightMask;
+    faces[2]&= (~rightMask);
+    unsigned int Rwhite = faces[0]&rightMask;
+    faces[2]|=Rwhite; // blue face done
+
+    temp = 0|((bitSelect(faces[3],3,0))<<16);
+    temp|= (bitSelect(faces[3],7,4)<<16);
+    temp|= (bitSelect(faces[3],31,28)>>16);
+    faces[0]&=(~rightMask);
+    faces[0]|=temp;// white face done
+
+    temp = 0|((bitSelect(faces[1],23,16))>>16);
+    temp|= (bitSelect(faces[1],15,12)<<16);
+    unsigned int greenmask = (1<<8)-1 - (1<<28);
+    faces[3]&=(~greenmask);
+    faces[3]|=temp;// green face done
+
+    faces[1]&=(~rightMask);
+    faces[1]|=Rblue;// yellow face done
+}
+
+void cube::R_prime(){
+    unsigned int temp = faces[4];
+    unsigned int rightMask = ((1<<24) - (1<<12));
+    faces[4] = faces[4]<<8;
+    temp = temp>>24;
+    faces[4] = temp|faces[4];// red center rotated
+
+    unsigned int Rblue = faces[2]&rightMask;
+    faces[2]&= (~rightMask);
+    unsigned int Ryellow = faces[1]&rightMask;
+    faces[2]|=Ryellow; // blue face done
+
+    temp = 0|((bitSelect(faces[3],3,0))<<16);
+    temp|= (bitSelect(faces[3],7,4)<<16);
+    temp|= (bitSelect(faces[3],31,28)>>16);
+    faces[1]&=(~rightMask);
+    faces[1]|=temp;// yellow face done
+
+    temp = 0|((bitSelect(faces[0],23,16))>>16);
+    temp|= (bitSelect(faces[0],15,12)<<16);
+    unsigned int greenmask = (1<<8)-1 - (1<<28);
+    faces[3]&=(~greenmask);
+    faces[3]|=temp;// green face done
+
+    faces[0]&=(~rightMask);
+    faces[0]|=Rblue;// white face done
+}
+
+void cube::R2(){
+    cube::R();
+    cube::R();
+}
+void cube::print(){
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<faces[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    cube C;
+    C.print();
+    C.R();
+    C.R_prime();
+    C.print();
+}
