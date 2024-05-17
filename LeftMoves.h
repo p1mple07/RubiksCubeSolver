c2b38cecdd954db2987ab09f9a5d7c6ad799fcd9
shashwat <shashwatskr2019@gmail.com>
2024-05-17 16:01:32 +0530

    left moves done

--- LeftMoves.h
+++ LeftMoves.h
@@ -3,59 +3,59 @@ using namespace std;
 
 
 void cube::L(){
-    unsigned int temp = faces[4];
-    unsigned int rightMask = ((1<<24) - (1<<12));
-    faces[4] = faces[4]>>8;
+    unsigned int leftMask = ((1<<8)-1 - (1<<28));
+    unsigned int temp = faces[5];
+
+    faces[5] = faces[5]>>8;
     temp = temp<<24;
-    faces[4] = temp|faces[4];// red center rotated
+    faces[5] = temp|faces[5];// orange center rotated
 
-    unsigned int Rblue = faces[2]&rightMask;
-    faces[2]&= (~rightMask);
-    unsigned int Rwhite = faces[0]&rightMask;
-    faces[2]|=Rwhite; // blue face done
+    unsigned int Lblue = faces[2]&leftMask;
+    faces[2]&= (~leftMask);
+    unsigned int Lyellow = faces[1]&leftMask;
+    faces[2]|=Lyellow; // blue face done
 
-    temp = 0|((bitSelect(faces[3],3,0))<<16);
-    temp|= (bitSelect(faces[3],7,4)<<16);
-    temp|= (bitSelect(faces[3],31,28)>>16);
-    faces[0]&=(~rightMask);
-    faces[0]|=temp;// white face done
+    temp = 0|((bitSelect(faces[3],15,12))<<16);
+    temp|= (bitSelect(faces[3],23,16)>>16);
+    faces[1]&=(~leftMask);
+    faces[1]|=temp;// yellow face done
 
-    temp = 0|((bitSelect(faces[1],23,16))>>16);
-    temp|= (bitSelect(faces[1],15,12)<<16);
-    unsigned int greenmask = (1<<8)-1 - (1<<28);
+    temp = 0|((bitSelect(faces[0],31,28))>>16);
+    temp|= (bitSelect(faces[0],7,0)<<16);
+    unsigned int greenmask = (1<<24) - (1<<12);
     faces[3]&=(~greenmask);
     faces[3]|=temp;// green face done
 
-    faces[1]&=(~rightMask);
-    faces[1]|=Rblue;// yellow face done
+    faces[0]&=(~leftMask);
+    faces[0]|=Lblue;// white face done
 }
 
 void cube::L_prime(){
-    unsigned int temp = faces[4];
-    unsigned int rightMask = ((1<<24) - (1<<12));
-    faces[4] = faces[4]<<8;
+    unsigned int leftMask = ((1<<8)-1 - (1<<28));
+    unsigned int temp = faces[5];
+
+    faces[5] = faces[5]<<8;
     temp = temp>>24;
-    faces[4] = temp|faces[4];// red center rotated
+    faces[5] = temp|faces[5];// orange center rotated
 
-    unsigned int Rblue = faces[2]&rightMask;
-    faces[2]&= (~rightMask);
-    unsigned int Ryellow = faces[1]&rightMask;
-    faces[2]|=Ryellow; // blue face done
+    unsigned int Lblue = faces[2]&leftMask;
+    faces[2]&= (~leftMask);
+    unsigned int Lwhite = faces[0]&leftMask;
+    faces[2]|=Lwhite; // blue face done
 
-    temp = 0|((bitSelect(faces[3],3,0))<<16);
-    temp|= (bitSelect(faces[3],7,4)<<16);
-    temp|= (bitSelect(faces[3],31,28)>>16);
-    faces[1]&=(~rightMask);
-    faces[1]|=temp;// yellow face done
+    temp = 0|((bitSelect(faces[3],15,12))<<16);
+    temp|= (bitSelect(faces[3],23,16)>>16);
+    faces[0]&=(~leftMask);
+    faces[0]|=temp;// white face done
 
-    temp = 0|((bitSelect(faces[0],23,16))>>16);
-    temp|= (bitSelect(faces[0],15,12)<<16);
-    unsigned int greenmask = (1<<8)-1 - (1<<28);
+    temp = 0|((bitSelect(faces[1],31,28))>>16);
+    temp|= (bitSelect(faces[1],7,0)<<16);
+    unsigned int greenmask = (1<<24) - (1<<12);
     faces[3]&=(~greenmask);
     faces[3]|=temp;// green face done
 
-    faces[0]&=(~rightMask);
-    faces[0]|=Rblue;// white face done
+    faces[1]&=(~leftMask);
+    faces[1]|=Lblue;// yellow face done
 }
 
 void cube::L2(){
